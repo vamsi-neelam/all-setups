68ef1c4c21b3e9a97abf432a6882be4612ca9d9e
vamsi-neelam <123477849+vamsi-neelam@users.noreply.github.com>
2026-09-01 19:59:59 +0530

    Add git installation to jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
-yum install java-21-amazon-corretto -y
+yum install java-21-amazon-corretto git -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 yum install jenkins -y
 systemctl start jenkins
